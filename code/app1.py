bfdc3edcba9a105d882da07e63eabe25e8715337
Dhruvin3103 <dcchawda2003@gmail.com>
2024-04-26 14:32:29 +0530

    made a app1.py containing all the components

--- code/app1.py
+++ code/app1.py
@@ -5,16 +5,30 @@ import requests
 from io import BytesIO
 import tempfile
 from gradio_client import Client
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
+from test4_streamlit import scrape_from_bewakoof,scrape_from_amazon,scrape_from_ajio
+import time
+from decouple import config
+import cloudinary.uploader
 
+
+class MultiViewDiffusionModel:
+    def __init__(self):
+        self.client = Client("dylanebert/multi-view-diffusion")
+    def make_prediction(self, image_url, text_input):
+        result = self.client.predict(image_url, text_input, api_name="/image_to_mv")
+        output_image = Image.open(result)
+        return output_image
+replicate = replicate.Client(api_token=config('REPLICATE_API_TOKEN'))
 # Set page title
 st.set_page_config(page_title="SOTA VTON")
-
 # Title
 st.title("State Of The Art VTON")
-
 # Tabs
-tabs = st.tabs(["VTON", "BetterLook"])
-
+tabs = st.tabs(["VTON", "Garment Search", "betterlook","multiview"])
 # VTON Tab
 with tabs[0]:
     # Sidebar
@@ -84,8 +98,53 @@ with tabs[0]:
             # Display output image
             output_area.image(output_image, use_column_width=True, caption="Output Image")
 
-# BetterLook Tab
+# Garment Search Tab
 with tabs[1]:
+    st.title("Garment Search")
+
+    search_query = st.text_input("Enter search query", "shirt")
+    website = st.radio("Select Website", ("Bewakoof", "Amazon", "Ajio"))
+    more_images = st.checkbox("More Images", False)
+
+    if st.button("Search"):
+        with st.spinner("Searching for garments..."):
+            if website == "Bewakoof":
+                results = scrape_from_bewakoof(search_query, more_images)
+            elif website == "Amazon":
+                results = scrape_from_amazon(search_query, more_images)
+            elif website == "Ajio":
+                results = scrape_from_ajio(search_query, more_images)
+
+            images = []
+            for data in results:
+                response = requests.get(data['src'])
+                img = Image.open(BytesIO(response.content))
+                images.append((img, data['name'], data['rating'], data['price']))
+
+            if not images:
+                st.warning("No data was scraped.")
+            else:
+                cols = st.columns(5)  # Display 5 images per row
+                for i, (img, name, rating, price) in enumerate(images):
+                    with cols[i % 5]:
+                        st.image(img, caption=f"{name}, {rating}⭐, {price}", use_column_width=True, output_format='JPEG')
+                        if st.button(label="Try on", key=i):
+                            st.write(f"You clicked on image {name}!")
+
+                            # Add code to set the selected image as the garment image in the VTON tab
+                            with tempfile.NamedTemporaryFile(delete=False, suffix=".png") as tmp_garment_img:
+                                img.save(tmp_garment_img.name)
+                                garment_img_path = tmp_garment_img.name
+
+                            with tabs[0].sidebar.form("garment_image_form"):
+                                st.image(img, caption="Selected Garment Image", use_column_width=True)
+                                submit_button = st.form_submit_button("Use Selected Image")
+
+                            if submit_button:
+                                tabs[0].sidebar.upload_state.value = img
+
+# BetterLook Tab
+with tabs[2]:
     # Display output image from VTON tab
     if 'output_img_path' in locals():
         st.image(Image.open(output_img_path), use_column_width=True, caption="Output Image")
@@ -97,11 +156,37 @@ with tabs[1]:
         with st.spinner("Enhancing the image..."):
             client = Client("dylanebert/multi-view-diffusion")
             result = client.predict(
-                output_img_path, # File path of the output image
+                output_img_path,  # File path of the output image
                 "a model wearing clothes for a photoshoot",
                 api_name="/image_to_mv"
             )
             enhanced_image = Image.open(result)
 
-            # Display enhanced image
-            st.image(enhanced_image, caption="Enhanced Image", use_column_width=True)
+        # Display enhanced image
+        st.image(enhanced_image, caption="Enhanced Image", use_column_width=True)
+
+
+with tabs[3]:
+    st.title("Multi-View Diffusion")
+
+    model = MultiViewDiffusionModel()
+
+    uploaded_image = st.file_uploader("Upload Image", type=["jpg", "jpeg", "png"])
+    if uploaded_image is not None:
+        cloudinary.config(
+            cloud_name=config('CLOUD_NAME'),
+            api_key=config('API_KEY'),
+            api_secret=config('API_SECRET')
+        )
+        upload_result = cloudinary.uploader.upload(uploaded_image)
+        text_input = 'fashion model standing for a photo shoot'
+
+        if st.button("Generate Output"):
+            if text_input:
+                image_url = upload_result["secure_url"]
+                output_image = model.make_prediction(image_url, text_input)
+                st.image(output_image, caption="Output Image", use_column_width=True)
+            else:
+                st.warning("Please provide a description.")
+    else:
+        st.warning("Please upload an image.")
